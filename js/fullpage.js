ce65da9d4dbf8313a8bf5d6a20a55f785a7b5610
bou2201 <lamchuc123456@gmail.com>
2022-09-22 10:52:17 +0700

    first commit- Demo section 1 slider

---
+++ js/fullpage.js
@@ -0,0 +1,9 @@
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  // scrollBar: true,
+  navigation: true,
+  navigationTooltips: ["Hero", "Category", "Featured", "Team"],
+  // showActiveTooltip: true,
+  responsiveWidth: 992,
+  scrollingSpeed: 700,
+});
