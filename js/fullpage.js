86f76bf6d15f917c0be2a9eab4f2500f7bca2acd
bou2201 <lamchuc123456@gmail.com>
2022-09-24 16:35:44 +0700

    demo section 2 & 3 homepage

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,9 +1,17 @@
 new fullpage("#fullpage", {
   autoScrolling: true,
+  lazyLoading: false,
   // scrollBar: true,
   navigation: true,
-  navigationTooltips: ["Hero", "Category", "Featured", "Team"],
+  navigationTooltips: ["Nổi Bật", "Danh Sách", "Top", "Team"],
   // showActiveTooltip: true,
   responsiveWidth: 992,
   scrollingSpeed: 700,
+  afterLoad: () => {
+    document
+      .querySelectorAll(".fp-table.active .aos-init")
+      .forEach((element) => {
+        element.classList.add("aos-animate");
+      });
+  }
 });
